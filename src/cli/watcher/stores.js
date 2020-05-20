b8c8100ae15e0b6a1dbcd6d65a197b5f316b4dd5
frederic2ec <freddy.cho@hotmail.com>
2020-05-20 23:39:04 +0000

    Lint V2

--- src/cli/watcher/stores.js
+++ src/cli/watcher/stores.js
@@ -4,29 +4,31 @@ const generateStores = require("../generator/stores");
 const consola = require("consola");
 
 module.exports = function routesWatcher(options, watcherBar) {
-    const config = require(Path.join(process.cwd(), ".dunejs/config"));
-    const srcDir = config.srcDir || options.config || "./";
+  const config = require(Path.join(process.cwd(), ".dunejs/config"));
+  const srcDir = config.srcDir || options.config || "./";
 
-    const appFolder = Path.join(process.cwd(), srcDir, "./store/");
+  const appFolder = Path.join(process.cwd(), srcDir, "./store/");
 
-    watcherBar.increment();
+  watcherBar.increment();
+  function onChange(options) {
+    consola.info("Store changed, rebuild started !");
+    generateStores(options, null);
+  }
 
-    chokidar
-        .watch(appFolder, {
-            ignoreInitial: true
-        })
-        .on("add", (event, path) => {
-            onChange(options);
-        }).on("unlink", (event, path) => {
-            onChange(options);
-        }).on("unlinkDir", (event, path) => {
-            onChange(options);
-        }).on("addDir", (event, path) => {
-            onChange(options);
-        });
-
-    function onChange(options) {
-        consola.info("Store changed, rebuild started !");
-        generateStores(options, null);
-    }
+  chokidar
+    .watch(appFolder, {
+      ignoreInitial: true,
+    })
+    .on("add", (event, path) => {
+      onChange(options);
+    })
+    .on("unlink", (event, path) => {
+      onChange(options);
+    })
+    .on("unlinkDir", (event, path) => {
+      onChange(options);
+    })
+    .on("addDir", (event, path) => {
+      onChange(options);
+    });
 };
